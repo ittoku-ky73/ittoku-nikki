0743283683d3f133299b27eb0a7948775825098b
いっとく <73953416+ittoku703@users.noreply.github.com>
2022-08-10 02:01:00 +0900

    APIを使ったテストコード

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.create!([
+    {
+        title: 'Next.js + Ruby on Rails + Dockerでブログサイトを作る'
+    },
+    {
+        title: 'Reactはよくわからない'
+    },
+    {
+        title: 'Ruby on Railsなら多少わかる'
+    },
+    {
+        title: 'TailwindCSSは使わない'
+    }
+]);
